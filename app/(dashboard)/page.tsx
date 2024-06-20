2954d9af19768aea40308a461741ba2758b949ee
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-06-20 17:23:06 +0100

    created empty org component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,12 @@
+"use client";
+import { EmptyOrg } from "./_components/empty-org";
+
 const Dashboardpage = () => {
-  return <div>Dashboard root page</div>;
+  return (
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      <EmptyOrg />
+    </div>
+  );
 };
 
 export default Dashboardpage;
