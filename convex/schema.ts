155c5c1182d30660ecaf6051914d92841226c6c5
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-06-23 22:05:16 +0100

    making schema

---
+++ convex/schema.ts
@@ -0,0 +1,12 @@
+import { v } from "convex/values";
+import { defineSchema, defineTable } from "convex/server";
+
+export default defineSchema({
+  boards: defineTable({
+    title: v.string(),
+    orgId: v.string(),
+    authorId: v.string(),
+    authorName: v.string(),
+    imaggeUrl: v.string(),
+  }),
+});
