89a58b35e29de7a09fca43e419c00108a8db7fe7
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-06-25 11:46:38 +0100

    used create board api

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,24 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useOrganization } from "@clerk/nextjs";
 
 export const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const createBoard = useMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+
+    createBoard({
+      orgId: organization.id,
+      title: "Untitled board",
+    });
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image src="/note.svg" alt="No results found" width={110} height={110} />
@@ -10,7 +27,9 @@ export const EmptyBoards = () => {
         Start by creating a board for your workflow
       </p>
       <div className="mt-6">
-        <Button size="lg">Create board</Button>
+        <Button onClick={onClick} size="lg">
+          Create board
+        </Button>
       </div>
     </div>
   );
