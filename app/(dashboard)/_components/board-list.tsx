a46b7c7b1e33919fd744d92082c5827f8fd3fe21
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-07-07 16:59:13 +0100

    updated lading state for board list

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -26,7 +26,7 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
           {query.favorites ? "Favorite Boards" : "Team Boards"}
         </h2>
         <div className="grid sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
-          <BoardCard.Skeleton />
+          <NewBoardButton orgId={orgId} disabled />
           <BoardCard.Skeleton />
           <BoardCard.Skeleton />
           <BoardCard.Skeleton />
