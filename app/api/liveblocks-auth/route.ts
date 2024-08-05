a7e1b921fb3eb512311d1f11ba2f4bfe247ba6b5
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-08-05 13:02:59 +0100

    api to link liveblocks with convex started

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -1,1 +1,23 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
 import { Liveblocks } from "@liveblocks/node";
+import { ConvexHttpClient } from "convex/browser";
+
+import { api } from "@/convex/_generated/api";
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+const liveblocks = new Liveblocks({
+  secret: process.env.LIVEBLOCKS_SECRET!,
+});
+
+export async function POST(request: Request) {
+  const authorization = await auth();
+  const user = await currentUser();
+
+  if (!authorization || !user) {
+    return new Response("Unauthorized", { status: 403 });
+  }
+
+  const { room } = await request.json();
+  const board = await convex.query(api.board.get, { id: room });
+}
