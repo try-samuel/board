1243f64eee43b51d3e5eefca18f1caabb8da6aff
try-samuel <fiyinfoluwasamuel580@gmail.com>
2024-12-16 10:35:12 +0100

    user avatar completed for active useres on a current board

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,29 +1,47 @@
 "use client";
 
+import { connectionIdToColor } from "@/lib/utils";
 import { useOthers, useSelf } from "@/liveblocks.config";
 import { UserAvatar } from "./user-avatar";
 
-const MAX_PARTICIPANTS = 3;
+const MAX_SHOWN_PARTICIPANTS = 1;
 
 export const Participants = () => {
   const users = useOthers();
   const currentUser = useSelf();
-
-  const hasMoreParticipants = users.length > MAX_PARTICIPANTS;
+  const hasMoreParticipants = users.length > MAX_SHOWN_PARTICIPANTS;
 
   return (
     <section className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
       <div className="flex gap-x-2">
-        {users.slice(0, MAX_PARTICIPANTS).map(({ connectionId, info }) => {
-          return (
-            <UserAvatar
-              key={connectionId}
-              src={info?.picture}
-              name={info?.name}
-              fallback={info?.name?.[0] || "T"}
-            />
-          );
-        })}
+        {users
+          .slice(0, MAX_SHOWN_PARTICIPANTS)
+          .map(({ connectionId, info }) => {
+            return (
+              <UserAvatar
+                borderColor={connectionIdToColor(connectionId)}
+                key={connectionId}
+                src={info?.picture}
+                name={info?.name}
+                fallback={info?.name?.[0] || "A"}
+              />
+            );
+          })}
+        {currentUser && (
+          <UserAvatar
+            borderColor={connectionIdToColor(currentUser.connectionId)}
+            src={currentUser.info?.picture}
+            name={currentUser.info?.name}
+            fallback={currentUser.info?.name?.[0] || "A"}
+          />
+        )}
+        {hasMoreParticipants && (
+          <UserAvatar
+            borderColor="transparent"
+            name={`+${users.length - MAX_SHOWN_PARTICIPANTS}`}
+            fallback={`+${users.length - MAX_SHOWN_PARTICIPANTS}`}
+          />
+        )}
       </div>
     </section>
   );
