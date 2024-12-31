85617b7824a199259e1ccb96d899590234558be5
try-samuel <fiyinfoluwasamuel580@gmail.com>
2024-12-31 16:21:29 +0100

    added text layer

---
+++ app/board/[boardId]/_components/text.tsx
@@ -0,0 +1,72 @@
+import { Kalam } from "next/font/google";
+import ComtentEditable, { ContentEditableEvent } from "react-contenteditable";
+
+import { cn, colorToCssColor } from "@/lib/utils";
+import { TextLayer } from "@/types/canvas";
+import { useMutation } from "@/liveblocks.config";
+
+const font = Kalam({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const calculateFontSize = (width: number, height: number) => {
+  const maxFontSize = 96;
+  const scaleFactor = 0.5;
+  const fontsizeBasedOnHeight = height * scaleFactor;
+  const fontsizeBasedOnWidth = width * scaleFactor;
+
+  return Math.min(fontsizeBasedOnHeight, fontsizeBasedOnWidth, maxFontSize);
+};
+
+interface TextProps {
+  id: string;
+  layer: TextLayer;
+  onPointerDown: (e: React.PointerEvent, id: string) => void;
+  selectionColor?: string;
+}
+
+export const Text = ({
+  id,
+  layer,
+  onPointerDown,
+  selectionColor,
+}: TextProps) => {
+  const { x, y, width, height, fill, value } = layer;
+
+  const updateValue = useMutation(({ storage }, newValue: string) => {
+    const liveLayers = storage.get("layers");
+
+    liveLayers.get(id)?.set("value", newValue);
+  }, []);
+
+  const handleContentChange = (e: ContentEditableEvent) => {
+    updateValue(e.target.value);
+  };
+
+  return (
+    <foreignObject
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+      onPointerDown={(e) => onPointerDown(e, id)}
+      style={{
+        outline: selectionColor ? `1px solid ${selectionColor}` : "none",
+      }}
+    >
+      <ComtentEditable
+        html={value || "Text"}
+        onChange={handleContentChange}
+        className={cn(
+          "h-full w-full flex items-center justify-center text-center drop-shadow-md outline-none",
+          font.className
+        )}
+        style={{
+          color: fill ? colorToCssColor(fill) : "#000",
+          fontSize: calculateFontSize(width, height),
+        }}
+      />
+    </foreignObject>
+  );
+};
